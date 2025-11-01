5f6e853d41c80b24edfd2c5f4685c39a27195449
mrubin01 <mi.rubino@protonmail.com>
2025-11-01 13:00:00 +0000

    Add global variables

--- main.py
+++ main.py
@@ -15,3 +15,14 @@ user_agent = functions.create_user_agent()
 CLEAR_CACHE = False
 if CLEAR_CACHE:
     user_agent.cache.clear()
+
+CALL = 1  # 1 call, 2 put
+STOCK_EXCHANGE = 2  # 1 NYSE, 2 NASDAQ, 3 ARCA
+SCOPE = 1  # 1 whole ticker list 2 only tickers with options
+WRITE_TICKERS_TO_FILE = 0
+MIN_BID_PRICE = 0.2
+TREND = -1  # -1 no trend, 1 downtrend, 2 uptrend
+HAVE_OPTIONS = 0  # 0 no active options, 1 with active options
+STOCK_PRICE = 25
+
+
