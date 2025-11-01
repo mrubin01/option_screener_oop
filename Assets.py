ef6a9c1782e58001da7ab81f0c82ab7a29fa18b3
mrubin01 <mi.rubino@protonmail.com>
2025-11-01 18:39:53 +0000

    Initialize child class Equity

--- Assets.py
+++ Assets.py
@@ -13,4 +13,39 @@ class Asset(object):
     def __str__(self):
         return f"{self.symbol} is an asset"
 
+    @property
+    def symbol(self):
+        return self._symbol
+
+    @symbol.setter
+    def symbol(self, new_symbol: str):
+        if isinstance(new_symbol, str):
+            self._symbol = new_symbol
+        else:
+            print("Invalid symbol! It must be a string")
+
+
+class Equity(Asset):
+    # override constructor
+    def __init__(self, symbol, exchange="Unknown"):
+        super(Equity, self).__init__(symbol)
+
+        self.exchange = exchange
+
+    def __str__(self):
+        return f"{self.symbol} is an equity, its exchange is {self.exchange}"
+
+    @property
+    def exchange(self):
+        return self._exchange
+
+    @exchange.setter
+    def exchange(self, new_exchange: str):
+        if isinstance(new_exchange, str):
+            self._exchange = new_exchange
+        else:
+            print("Invalid exchange! It must be a string")
+
+
+
 
